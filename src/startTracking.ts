2e3692ea311beddb086bb53542e3b040579e7725
yohanse <mehabawyohanse793@gmail.com>
2024-12-28 23:43:25 +0300

    integrate with authenticateAndCreateRepo

--- src/startTracking.ts
+++ src/startTracking.ts
@@ -1,9 +1,11 @@
 import * as vscode from 'vscode';
+import authenticateAndCreateRepo from './authenticateAndCreateRepo';
 
 export default async () => {
     const choice = await vscode.window.showInformationMessage("We need to create a GitHub repository called 'DevDiary Tracking' to store your tracking logs. Do you want to proceed?", "Yes", "No");
     if (choice === "Yes") {
         vscode.window.showInformationMessage("Creating a GitHub repository called 'DevDiary Tracking'...");
+        await authenticateAndCreateRepo();
     }
     else {
         vscode.window.showInformationMessage("You have chosen not to proceed with tracking.");
