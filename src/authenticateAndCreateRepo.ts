58a12900912bdabc9af7112428e5f4ff9e37ab82
yohanse <mehabawyohanse793@gmail.com>
2024-12-28 23:42:54 +0300

    implementing a way to get github access token

---
+++ src/authenticateAndCreateRepo.ts
@@ -0,0 +1,17 @@
+import * as vscode from 'vscode';
+export default async () => {
+    try {
+        // Request GitHub authentication with the necessary scope
+        const session = await vscode.authentication.getSession("github", ["repo"], { createIfNone: true });
+    
+        if (session) {
+          vscode.window.showInformationMessage("GitHub authentication successful!");
+          // Use the access token to create the repository
+          await createTrackingRepo(session.accessToken);
+        } else {
+          vscode.window.showErrorMessage("Authentication failed or canceled.");
+        }
+      } catch (error) {
+        vscode.window.showErrorMessage(`Authentication error: ${error}`);
+      }
+};
