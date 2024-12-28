6b5dda4b94639f7d94e10e80613cee78977cf135
yohanse <mehabawyohanse793@gmail.com>
2024-12-28 22:01:27 +0300

    expermenting: Give the Hello World command a new name in the Command Palette

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('devdiary.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('devdiary.nono', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DevDiary!');
+		vscode.window.showInformationMessage('Hello Vs Code');
 	});
 
 	context.subscriptions.push(disposable);
