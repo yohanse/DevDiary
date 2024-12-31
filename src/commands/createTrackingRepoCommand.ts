f9e2fbe2d9c8b07eef5a88bec64200ada3e162b8
yohanse <mehabawyohanse793@gmail.com>
2025-01-01 01:14:28 +0300

    build the user interface for creating repository

--- src/commands/createTrackingRepoCommand.ts
+++ src/commands/createTrackingRepoCommand.ts
@@ -1,9 +1,20 @@
 import * as vscode from "vscode";
 import inputRepositoryName from "../ui/inputRepositoryName";
+import createTrackingRepoController from "../controllers/createTrackingRepoController";
+import chooseRepoVisibility from "../ui/chooseRepoVisibility";
 
 export default async () => {
     const repoName = await inputRepositoryName();
-    if (repoName){
-        await vscode.window.showInformationMessage(repoName);
+    if (repoName) {
+        const visibility = await chooseRepoVisibility();
+        if (visibility) {
+            try {
+                await createTrackingRepoController(repoName, visibility === "Private");
+                vscode.window.showInformationMessage("Repository created successfully");
+            }
+            catch (error) {
+                vscode.window.showErrorMessage(`Error creating repository`);
+            }
+        }
     }
 };  
