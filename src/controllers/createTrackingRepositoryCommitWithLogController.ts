511bc39187edb600172e2c32382603ec807fbf91
yohanse <mehabawyohanse793@gmail.com>
2025-01-01 00:29:59 +0300

    integrate create github tree on the first step

--- src/controllers/createTrackingRepositoryCommitWithLogController.ts
+++ src/controllers/createTrackingRepositoryCommitWithLogController.ts
@@ -2,20 +2,31 @@ import * as vscode from "vscode";
 import fetchAuthenticationSessionController from "./fetchAuthenticationSessionController";
 import retrieveTrackingRepositorySettings from "../helpers/retrieveTrackingRepositorySettings";
 import getTrackingRepoReferenceController from "./getTrackingRepoReferenceController";
+import createTrackingRepositoryTreeController from "./createTrackingRepositoryTreeController";
 
-export default async() => {
+export default async () => {
     const session: vscode.AuthenticationSession = await fetchAuthenticationSessionController();
     const { repoOwner, repoName, defaultBranch } = retrieveTrackingRepositorySettings();
 
-    
+
 
     if (session && repoOwner && repoName && defaultBranch) {
 
-        // Get a reference to the tracking repository
-        const currentCommitSha: string = await getTrackingRepoReferenceController(session.accessToken, repoOwner, repoName, defaultBranch);
+        try {
+            // Step 1. Get a reference to the tracking repository
+            const refCommitSha: string = await getTrackingRepoReferenceController(session.accessToken, repoOwner, repoName, defaultBranch);
+
+            // Step 2. Create a Tree with the log file content
+            const logContent = "Log content here";
+            const treeCommitSha = await createTrackingRepositoryTreeController(session.accessToken, repoOwner, repoName, logContent, refCommitSha);
+            
+
+        }   
+        catch (error) {
+            throw new Error(`Error getting tracking repository reference`);
+        }
     }
     else {
         throw new Error("Authentication session or repository settings not found. Please ensure you are signed in and have a repository set up.");
     }
-    
 };
