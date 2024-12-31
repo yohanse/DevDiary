868ef31c3b8f81e6cc90541511bd33d54065f6d0
yohanse <mehabawyohanse793@gmail.com>
2024-12-31 15:25:48 +0300

    implement create tracking repo with session

---
+++ src/controllers/createTrackingRepoControllerWithSession.ts
@@ -0,0 +1,18 @@
+import * as vscode from "vscode";
+import { GitHubRepository } from "../interfaces/githubRepository";
+
+export default async (repoName: string, accessToken: string, isPrivate: boolean): Promise<GitHubRepository> => {
+    const { Octokit } = await import("@octokit/rest");
+    const octokit = new Octokit({ auth: accessToken });
+
+    try {
+        const response = await octokit.repos.createForAuthenticatedUser({
+            name: repoName,
+            description: "A repository to store your activity logs.",
+            private: isPrivate,
+        });
+        return response.data as GitHubRepository;
+    } catch (error) {
+        throw error;
+    }
+};
