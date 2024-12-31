8d01aba343d3ddf20ba06431b37f2ff84e6c5ffd
yohanse <mehabawyohanse793@gmail.com>
2024-12-31 23:44:04 +0300

    Implement helper function to retrieve repo settings

---
+++ src/helpers/retrieveTrackingRepositorySettings.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+export default async () => {
+    const repoOwner = vscode.workspace.getConfiguration().get<string>('devdiary.repoOwner');
+    const repoName = vscode.workspace.getConfiguration().get<string>('devdiary.repoName');
+    const defaultBranch = vscode.workspace.getConfiguration().get<string>('devdiary.defaultBranch');
+    return { repoOwner, repoName, defaultBranch };
+};
+
