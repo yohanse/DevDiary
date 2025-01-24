b0d87bf8ac07a16b146cf30c72517885c1cde197
yohanse <mehabawyohanse793@gmail.com>
2025-01-24 04:38:11 +0300

    Requestion to the gemini api every 30 minute

--- src/commands/startActivityTrackingCommand.ts
+++ src/commands/startActivityTrackingCommand.ts
@@ -1,5 +1,9 @@
 import * as vscode from "vscode";
 import { getTrackingStatus, setTrackingInterval, setTrackingStatus } from "../helpers/state";
+import { getFileActivities, setFileActivities } from "../controllers/activityTrackerController";
+import aiPostController from "../controllers/aiPostController";
+import create from "../controllers/createTrackingRepoCommitController";
+import createTrackingRepositoryCommitWithLogController from "../controllers/createTrackingRepositoryCommitWithLogController";
 
 export default async () => {
     if(getTrackingStatus()) {
@@ -8,7 +12,17 @@ export default async () => {
     else {
         setTrackingStatus(true);
 
-        const trackingInterval: NodeJS.Timeout = setInterval(() => vscode.window.showInformationMessage("Tracking ....."), 500);
+        const trackingInterval: NodeJS.Timeout = setInterval(async () => {
+            const fileActivities = getFileActivities();
+            try {
+                console.log(fileActivities);
+                const response = await aiPostController(fileActivities);
+                await createTrackingRepositoryCommitWithLogController(response);
+            } catch (error) {
+                vscode.window.showErrorMessage("There was an error while posting the data to the AI model");
+            }
+            setFileActivities();
+        }, 60000);
         setTrackingInterval(trackingInterval);
         vscode.window.showInformationMessage("Activity tracking started");
     }
